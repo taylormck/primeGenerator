8584e06834c1c6ed8e959dc62b83df0353ba5eb0
Taylor McKinney <taylormck@utexas.edu>
2013-08-08 12:53:06 -0500

    Started on a sieve table

--- primeGenerator.rb
+++ primeGenerator.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby -wKU
 # Prime Generator
+require 'set' # set
 
 # Set the bounds of the range to be valid
 def setBoundsOnRange(range)
@@ -24,8 +25,16 @@ end
 # @return true if n is prime
 #         false otherwise
 def isPrime(n)
+  # Special case where n is even
+  if n == 2
+    return true
+  elsif n % 2 == 0
+    return false
+  end
+
   sqrt_n = Math.sqrt(n)
-  for i in 2..sqrt_n
+  numbersToCheck = 3..sqrt_n
+  numbersToCheck.step(2) do |i|
     if n % i == 0
       return false
     end
@@ -33,11 +42,36 @@ def isPrime(n)
   return true
 end
 
+# Takes in a range and returns a list of numbers that
+# should be checked for primality, stripping numbers
+# that are multiples of numbers earlier in the range
+def getListOfNumbersToCheck(range)
+  list = *(range[0]..range[1])
+
+  i = 0
+  while i < list.length
+    smallerValue = list[i]
+    j = i + 1
+    while j < list.length
+      largerValue = list[j]
+      if largerValue % smallerValue == 0
+        list.delete_at(j)
+      elsif
+        j += 1
+      end
+    end
+    i += 1
+  end
+
+  return list
+end
+
 # Read in the next line and print the primes the results
 def runCase
   range = readLineIntoArray
+  numbersToCheck = getListOfNumbersToCheck(range)
 
-  for i in range[0]..range[1]
+  for i in numbersToCheck
     if isPrime(i)
       print i, "\n"
     end
